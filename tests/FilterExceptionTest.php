25efdefabd4f82cdae6993e8908696b9366cdb26
Chad Gray <chad.gray@traderinteractive.com>
2018-03-05 12:22:50 -0500

    Add PHPUnit

---
+++ tests/FilterExceptionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TraderInteractiveTest\Exceptions;
+
+use PHPUnit\Framework\TestCase;
+use TraderInteractive\Exceptions\FilterException;
+
+/**
+ * @coversDefaultClass TraderInteractive\Exceptions\FilterException
+ */
+final class ExceptionTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function basicUsage()
+    {
+        $this->assertSame('the message', (new FilterException('the message'))->getMessage());
+    }
+}
